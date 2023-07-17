5b19e1a3b93d550de9b8ca70e0358aa91e36fbad
VankoLeonov <vankoleonov@abv.bg>
2023-07-17 17:11:45 +0300

    Fixed CompanyAdminViewModel bug

--- Attendance_Management_System/ViewModels/CompanyAdminViewModel.cs
+++ Attendance_Management_System/ViewModels/CompanyAdminViewModel.cs
@@ -3,54 +3,30 @@ using Attendance_Management_System.Models;
 using Attendance_Management_System.Views;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Attendance_Management_System.ViewModels
 {
     public class CompanyAdminViewModel : INotifyPropertyChanged
     {
 
-        private ObservableCollection<Employee> _employees;
-        private string _connectionString = "dbConnect"; 
-
-        public ObservableCollection<Employee> Employees
-        {
-            get { return _employees; }
-            set
-            {
-                _employees = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Employees)));
-            }
-        }
-
-        public RelayCommand AddEmployeeCommand { get; }
-        public RelayCommand EditEmployeeCommand { get; }
-        public RelayCommand DeleteEmployeeCommand { get; }
-
+        public ICommand OpenViewCommand { get; }
         public CompanyAdminViewModel()
         {
-            AddEmployeeCommand = new RelayCommand(AddEmployee);
-            EditEmployeeCommand = new RelayCommand(EditEmployee, CanEditOrDeleteEmployee);
-            DeleteEmployeeCommand = new RelayCommand(DeleteEmployee, CanEditOrDeleteEmployee);
-
-            Employees = new ObservableCollection<Employee>();
-
-            LoadEmployees();
+            OpenViewCommand = new RelayCommand(OpenView);
         }
 
         private void OpenView(object parameter)
         {
-            
 
-                EmployeeManagment empman = new EmployeeManagment();
-                empman.Show();
 
-                Application.Current.MainWindow?.Close();
-            
+            EmployeeManagementView empman = new EmployeeManagementView();
+            empman.Show();
+
+            Application.Current.MainWindow?.Close();
+
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
