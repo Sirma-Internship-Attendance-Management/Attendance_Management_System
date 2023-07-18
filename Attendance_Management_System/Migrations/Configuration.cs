7421829f9841e9630717198dbbd7f4d7877baf2d
VankoLeonov <vankoleonov@abv.bg>
2023-07-18 17:49:31 +0300

    Added migration for our project

---
+++ Attendance_Management_System/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Attendance_Management_System.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Attendance_Management_System.DataAccess.MyDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Attendance_Management_System.DataAccess.MyDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
