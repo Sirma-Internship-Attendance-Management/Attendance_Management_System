2716c7ce2ca0fa080f14d97ced63d534c2692814
VankoLeonov <vankoleonov@abv.bg>
2023-07-19 20:32:23 +0300

    Fixed bug with Employee table visualization.

--- Attendance_Management_System/Views/EmployeeManagementView.xaml.cs
+++ Attendance_Management_System/Views/EmployeeManagementView.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using Attendance_Management_System.ViewModels;
 namespace Attendance_Management_System.Views
 {
     /// <summary>
@@ -20,8 +20,9 @@ namespace Attendance_Management_System.Views
     public partial class EmployeeManagementView : Window
     {
         public EmployeeManagementView()
-        {
+        {            
             InitializeComponent();
+            DataContext = new EmployeeManagementViewModel();
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
