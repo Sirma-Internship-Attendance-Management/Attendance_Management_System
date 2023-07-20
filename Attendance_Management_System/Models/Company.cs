c2e84af3f7fea6f3a6515a0e6b7c7819c43de797
IvanAleksandr0 <ivan.georgiev25vt@gmail.com>
2023-07-20 17:09:36 +0300

    Company Model Final

--- Attendance_Management_System/Models/Company.cs
+++ Attendance_Management_System/Models/Company.cs
@@ -10,16 +10,13 @@ namespace Attendance_Management_System.Models
     {
         public int CompanyId { get; set; }
         public string CompanyName { get; set; }
-        public string AdminUsername { get; set; }
+        public string? Type { get; set; }
         
-        public string AdminPassword { get; set; }
-        
-        /*  //new Information Added
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Address { get; set; }
+        public string? Website { get; set; }
 
-        public byte[]? Logo { get; set; }*/
+        public string? Address { get; set; }       
+
+        public byte[]? Logo { get; set; }
     }
 
 }
